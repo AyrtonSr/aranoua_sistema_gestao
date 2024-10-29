ef112ae8f820886fb4a182d531f8ae132eaa83f2
AyrtonSr <133722471+AyrtonSr@users.noreply.github.com>
2024-10-29 07:21:35 -0400

    Inicio do projeto
    Dando inicio a atividade avaliativa do projeto Aranoua Spring Framework

---
+++ sistema_gestao/src/test/java/edu/ifam/aranoua/sistema_gestao/SistemaGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifam.aranoua.sistema_gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
